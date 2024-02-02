d9f26711aef043a77cdf063277ea839271cc8c5a
Zohar Cochavi <cochavi.zohar@protonmail.com>
2024-02-02 13:28:48 +0100

    Fix lmbff environment issue
    The package certifi was causing issues because it was clashing with the required version, but could not be uninstalled as it was a 'distutils' package. I've added the '--ignore-installed' parameter and it seems to be fixed.

--- src/replicating_ifh/fine-tuners-setup/lmbff.sh
+++ src/replicating_ifh/fine-tuners-setup/lmbff.sh
@@ -1,14 +1,14 @@
 # #!/usr/bin/bash
 
 # clone the repo if it doesn't exist
-if ! [ -f LM_BFF ]; then
+if ! [ -f "LM_BFF" ]; then
     git clone git@github.com:princeton-nlp/LM-BFF.git LM_BFF \
         || git clone https://github.com/princeton-nlp/LM-BFF.git LM_BFF
 fi
 
 cd LM_BFF && git pull 
 
-# create the conda environment if it doesn't exist
+# # create the conda environment if it doesn't exist
 conda env list | grep "lmbff" \
     && echo "Conda environment lmbff already exists" \
     || conda create -n lmbff -y python=3.6
@@ -19,14 +19,18 @@ echo -e "$(cat requirements.txt \
     | grep -v 'regex' \
     | grep -v 'certifi' \
 )" > requirements-min.txt
-conda run -n lmbff pip install -r requirements-min.txt
+conda run -n lmbff pip install -r requirements-min.txt \
+    --ignore-installed certifi # google colab has certifi has a distutils package
+                              # for _whatever reason_.
 
 # download the dataset 
 cd data
-if ! [ -f datasets.tar ]; then
-    chmod +x download_dataset.sh && ./download_dataset.sh
+if ! [ -f "original" ]; then
+    rm datasets.tar && echo "Removed old 'datasets.tar'"
+    chmod +x download_dataset.sh
+    ./download_dataset.sh
 fi
-cd -
+cd ..
 
 # generate the k-shot data
 conda run -n lmbff python tools/generate_k_shot_data.py
